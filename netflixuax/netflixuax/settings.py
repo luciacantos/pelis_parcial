f1657a2f738c1b55128efe09771514472205ddff
rubences <91731294+rubences@users.noreply.github.com>
2024-11-29 17:29:07 +0000

    mas cambios

--- netflixuax/netflixuax/settings.py
+++ netflixuax/netflixuax/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'authentication',
     'streaming',
 ]
 
@@ -70,6 +71,18 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
 
 LANGUAGE_CODE = 'es'
 TIME_ZONE = 'UTC'
--- netflixuax/netflixuax/urls.py
+++ netflixuax/netflixuax/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('streaming.urls')),
+    path('auth/', include('authentication.urls')),
 ]
 
 
