e8d5700d7db524c78b0d60ce8de6a7401c6da6c8
rubences <91731294+rubences@users.noreply.github.com>
2024-11-29 18:19:51 +0000

    cambios

---
+++ prueba.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "https://api.themoviedb.org/3/authentication"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMmRjMzIzYWI2NmU1NDk1YzI3NzkxZWE2NDY5ZTU1ZSIsIm5iZiI6MTczMjg5ODc5MC40MzcsInN1YiI6IjY3NDllZmU2MDkzNmI2ZTRmYjlmYTdhYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._1EC6VLF2wavTLLqKYSw4Ch5gMkekIPS40GTU8F4hXo"
+}
+
+response = requests.get(url, headers=headers)
+
+print(response.text)
