85a203385ab2d9d68b951da5a78b1068ea67bafa
rubences <91731294+rubences@users.noreply.github.com>
2024-11-29 17:04:17 +0000

    cambios

--- netflixuax/streaming/models.py
+++ netflixuax/streaming/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    release_date = models.DateField()
+    poster_url = models.URLField()
+    backdrop_url = models.URLField()
+    tmdb_id = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return self.title
+
--- netflixuax/streaming/views.py
+++ netflixuax/streaming/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from streaming.models import Movie
+from streaming.serializers import MovieSerializer
+
+class MovieListView(APIView):
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+
+class MovieDetailView(APIView):
+    def get(self, request, pk):
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
